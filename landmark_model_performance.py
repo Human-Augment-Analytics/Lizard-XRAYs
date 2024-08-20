e566697ea55a6809a36fb11795388392ace8b49f
Ayush Parikh <ayusoccer@gmail.com>
2024-08-20 09:09:04 -0700

    Add files via upload

--- landmark_model_performance.py
+++ landmark_model_performance.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import numpy as np
 import argparse
+import numpy as np
+from collections import defaultdict
 
 # maps to test to output
 # maps ground truth landmark number to model output landmark number
@@ -61,9 +63,24 @@ def parse_xml(file_path):
     
     return data
 
+def calculate_avg_ruler_length_pixels(testData):
+    distances = []
+    for image_file, test_parts in testData.items():
+        zero_coords = None
+        one_coords = None
+        for test_part_name, test_coords in test_parts.items():
+            if test_part_name == 1:
+                one_coords = test_coords
+            elif test_part_name == 0:
+                zero_coords = test_coords
+        diff_x = zero_coords[0] - one_coords[0]
+        diff_y = zero_coords[1] - one_coords[1]
+        diff = np.sqrt(diff_x**2 + diff_y**2)
+        distances.append(diff)
+    return np.mean(distances)
+
 def calculate_differences(outputData, testData):
-    differences = []
-    differencesMap = dict()
+    differencesMap = defaultdict(list)
 
     for image_file, output_parts in outputData.items():
         if image_file in testData:
@@ -76,27 +93,24 @@ def calculate_differences(outputData, testData):
                         diff_x = test_coords[0] - output_coords[0]
                         diff_y = test_coords[1] - output_coords[1]
                         diff = np.sqrt(diff_x**2 + diff_y**2)
-                        differences.append(diff)
-                        differencesMap[test_part_name] = round(diff, 2)
+                        differencesMap[test_part_name].append(round(diff, 2))
 
-    return differences, differencesMap
+    return differencesMap
 
 def main(output_xml, test_xml):
     # Parse the XML files
     output_data = parse_xml(output_xml)
     test_data = parse_xml(test_xml)
+
+    avg_ruler_length = calculate_avg_ruler_length_pixels(test_data)
+    conversion_factor = float(27 / avg_ruler_length)
     
     # Calculate the differences between corresponding landmarks
-    differences, differencesMap = calculate_differences(output_data, test_data)
-    print(differencesMap)
-    if differences:
-        average_difference = np.mean(differences)
-        average_deviation = np.std(differences)
-        
-        print(f"Average Difference: {average_difference:.2f} pixels")
-        print(f"Average Deviation: {average_deviation:.2f} pixels")
-    else:
-        print("No matching landmarks found between the files.")
+    differencesMap = calculate_differences(output_data, test_data)
+    
+    for landmark in differencesMap:
+        avg = round(np.mean(differencesMap[landmark]) * conversion_factor, 2)
+        print("Landmark = ", landmark, " Average = ", avg)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate the average difference and deviation between landmarks in two XML files.")
