ade39b0a3d08bdcb451f1e99832dc2edc331e162
m-a-quintana <mercedesantoniaquintana@gmail.com>
2024-11-14 21:41:02 -0800

    Add files via upload

---
+++ updated_files/inference.py
@@ -0,0 +1,52 @@
+import argparse
+import utils
+import ntpath
+
+ap = argparse.ArgumentParser()
+ap.add_argument(
+    "-i",
+    "--input-dir",
+    type=str,
+    default="images",
+    help="input directory (default = images)",
+    metavar="",
+)
+ap.add_argument(
+    "-p",
+    "--predictor",
+    type=str,
+    default="models/predictor.dat",
+    help="trained shape prediction model (default = models/predictor.dat)",
+    metavar="",
+)
+ap.add_argument(
+    "-o",
+    "--out-file",
+    type=str,
+    default="output.xml",
+    help="output file name suffix (default = output.xml)",
+    metavar="",
+)
+ap.add_argument(
+    "-l",
+    "--ignore-list",
+    nargs="*",
+    type=int,
+    default=None,
+    help=" (optional) prevents landmarks of choice from being output",
+    metavar="",
+)
+
+
+print(utils.predictions_to_xml)
+args = vars(ap.parse_args())
+print(vars(ap.parse_args()))
+utils.predictions_to_xml(
+    args["predictor"],
+    folder=args["input_dir"],
+    ignore=args["ignore_list"],
+    output=args["out_file"],
+)
+
+utils.dlib_xml_to_pandas(args["out_file"])
+utils.dlib_xml_to_tps(args["out_file"])
